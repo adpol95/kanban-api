45188a245e0f3695775fabcedb3290d71e868c74
adpol95 <adpol95@gmail.com>
2023-03-08 17:14:19 +0300

    port set only 5000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 const bodyParser1 = require("./bodyParser");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 bodyParser1(app);
 routes(app);
