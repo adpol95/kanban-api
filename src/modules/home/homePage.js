e1d9f506b874476ddff921a297f834f3295f7f27
adpol95 <adpol95@gmail.com>
2023-04-24 12:52:20 +0300

    Upgrade homePage.js

--- src/modules/home/homePage.js
+++ src/modules/home/homePage.js
@@ -1,5 +1,5 @@
 function homePage(req, res) {
-  res.status(200).json('Welcome to Kanban Server');
+  res.status(200).json('Welcome to Kanban Server. You can made CRUD operations with it. Send requests with path /card');
 }
 
 module.exports = homePage;
