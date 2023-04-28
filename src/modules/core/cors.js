55f93f46d90ebdb0cfbdbdcb0c6ce10535c53fc9
adpol95 <adpol95@gmail.com>
2023-04-28 16:51:31 +0300

    CORS FIX

--- src/modules/core/cors.js
+++ src/modules/core/cors.js
@@ -14,7 +14,7 @@ module.exports = function(app) {
   // });
 
   app.use(cors({
-    origin: "*",
+    origin: "https://fetchreacttest1-adpol95.b4a.run",
     allowedHeaders: ['Origin, X-Requested-With, Content-Type'],
     methods: ["GET", "POST", "PATCH", "DELETE"]
   }))
