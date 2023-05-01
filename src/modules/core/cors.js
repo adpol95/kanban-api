410d6efa71bdb2478bdadecb62ecc8620ecf6082
adpol95 <adpol95@gmail.com>
2023-05-01 13:22:30 +0300

    Change origin in CORS 7

--- src/modules/core/cors.js
+++ src/modules/core/cors.js
@@ -14,7 +14,7 @@ module.exports = function(app) {
   // });
 
   app.use(cors({
-    origin: "https://todolistv2-adpol95.b4a.run",
+    origin: "*",
     allowedHeaders: ['Origin, X-Requested-With, Content-Type'],
     methods: ["GET", "POST", "PATCH", "DELETE"]
   }))
